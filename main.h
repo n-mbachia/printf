366b9dc903a94acab111efe9c2500f82b35e1bce
n-mbachia <ndmbachia@outlook.com>
2022-05-17 21:29:01 +0000

    printf function

--- main.h
+++ main.h
@@ -4,10 +4,31 @@
 
 #ifndef _PRINTF_
 #define _PRINTF_
-// provided prototype
-int _printf(const char *format, ...);
+/**
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
+ */
+typedef struct print
+{
+	char *t;
+	int (*f)(va_list);
+} print_t;
 
-// putchar fucntion
 int _putchar(char c);
+int _printf(const char *format, ...);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
+int print_S(va_list S);
+int print_r(va_list r);
+int print_R(va_list R);
 
 #endif
