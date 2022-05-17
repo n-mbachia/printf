0731cacba7d5b6f73a559a4ae4b91811f239dd38
n-mbachia <ndmbachia@outlook.com>
2022-05-17 23:36:22 +0000

    printf project

--- _printf.c
+++ _printf.c
@@ -1,5 +1,6 @@
 #include "main.h"
-
+#include <stddef.h >
+#include <stdarg.h>
 /**
  * _printf - produces output according to a format
  * @format: format string containing the characters and the specifiers
@@ -8,40 +9,40 @@
  * specifiers contained into fmt
  * Return: length of the formatted output string
  */
-int _printf(const char *format, ...)
+int _printf(const char * format, ...)
 {
-	int (*pfunc)(va_list, flags_t *);
-	const char *p;
-	va_list arguments;
-	flags_t flags = {0, 0, 0};
+	va_lists args;
+	int i = 0, k = 0;
+	int *str = NULL;
 
-	register int count = 0;
+	va_start(args, format);
 
-	va_start(arguments, format);
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = format; *p; p++)
+	while (format[i] != '\0')
 	{
-		if (*p == '%')
+		if (format[i] != '%')
+			_putchar(format[i]);
+		else
 		{
-			p++;
-			if (*p == '%')
+			if (format[i+1] == 'c')
 			{
-				count += _putchar('%');
-				continue;
+				_putchar(va_arg(args, int));
+				i++;
 			}
-			while (get_flag(*p, &flags))
-				p++;
-			pfunc = get_print(*p);
-			count += (pfunc)
-				? pfunc(arguments, &flags)
-				: _printf("%%%c", *p);
-		} else
-			count += _putchar(*p);
+			else if (format[i+1] == 's')
+			{
+				i++;
+				str = va_arg(args, char *);
+				k = 0;
+			       	while (str[i] != '\0')
+				{
+					_putchar(str[k]);
+					k++;
+				}	
+			}
+		}
+		i++;
 	}
-	_putchar(-1);
-	va_end(arguments);
-	return (count);
+	va_and(args)
+
+	return (0);
 }
